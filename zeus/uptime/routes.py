442a575a5bddc5157005685fab03f379ced5b4ed
Nevil Macwan <macnev2013@gmail.com>
2021-03-21 11:35:57 +0000

    added integration for add website

--- zeus/uptime/routes.py
+++ zeus/uptime/routes.py
@@ -1,22 +1,26 @@
 from flask import redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
+import re
 from zeus import db
 from zeus.database.uptime import UptimeWebsiteDetails
 from zeus.uptime import bp
 from zeus.uptime.forms import UptimeAddSiteForm
 
+REGEX = '^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$'
 
 @login_required
 @bp.route('/', methods=['GET', 'POST'])
 def uptime():
     add_site_form = UptimeAddSiteForm(request.form)
-    action = request.form.get('action')
     if request.method == 'POST' and add_site_form.validate():
-        if action == 'save':
+        if request.form.get('action') == 'save':
             website = add_site_form.url.data
+            if not re.match(REGEX, website):
+                return redirect(url_for('uptime.uptime'))
             uptime_websites = UptimeWebsiteDetails(username=current_user.username, website=website)
             db.session.add(uptime_websites)
             db.session.commit()
+            add_site_form.url.data = ''
     website_list = UptimeWebsiteDetails.query.filter_by(username=current_user.username).all()
-    return render_template('uptime/uptime.html', page='uptime', form=add_site_form)
+    return render_template('uptime/uptime.html', page='uptime', form=add_site_form, website_list=website_list)
